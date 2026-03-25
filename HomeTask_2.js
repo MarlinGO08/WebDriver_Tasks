4fc5de2f8082704816909eab77b5fc62cd816b97
Olexander <shingm8991677@gmail.com>
2026-03-26 00:02:29 +0200

    Added scroll down and test of the footer link

--- HomeTask_2.js
+++ HomeTask_2.js
@@ -1,7 +1,7 @@
 import { browser, expect } from '@wdio/globals'
 
 describe('HomeTask_2', () => {
-    it('Go to the API page', async () => {
+    xit('Go to the API page', async () => {
         await browser.setWindowSize(1440, 1000);
         await browser.url('https://webdriver.io')
 
@@ -12,7 +12,23 @@ describe('HomeTask_2', () => {
         console.log('API navigation button is clickable: ',await   navApi.isClickable())
 
         await expect(navApi.isClickable())
+        await expect(navApi.isDisplayed())
         await navApi.click()
         await browser.pause(2000)
+    })
+
+    it('Scroll down and check footer link', async () => {
+        await browser.setWindowSize(1440, 1000);
+        await browser.url('https://webdriver.io/docs/api')
+
+        const footerButton = await $('li a[class="footer__link-item"][href="/docs/api"]')
+        await footerButton.scrollIntoView()
+        await browser.pause(2000)
+
+        await expect(footerButton.isClickable())
+        await expect(footerButton.isDisplayed())
+
+        //Get HTML of the link
+        console.log('HTML of the footer link', await footerButton.getHTML())
     })
 })
