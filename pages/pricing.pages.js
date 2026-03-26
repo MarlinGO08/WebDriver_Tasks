b0d1e7a383452973344925555af1a86842589eb1
Olexander <shingm8991677@gmail.com>
2026-03-26 21:15:18 +0200

    Added pricing.pages.js

---
+++ pages/pricing.pages.js
@@ -0,0 +1,6 @@
+class PricingPage {
+    get freeTrialBtn() {return $('/html/body/div[1]/div[5]/main/div[2]/div/div[1]/div[3]/div/div/div/div[3]/div/div/div[1]/a')}
+
+}
+
+export default new PricingPage()
