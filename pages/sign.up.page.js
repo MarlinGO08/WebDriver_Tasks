d8c391299e5a836b89da9b35e33f6f83187f6fe2
Olexander <shingm8991677@gmail.com>
2026-03-26 20:12:12 +0200

    Added sign.up.page.js

---
+++ pages/sign.up.page.js
@@ -0,0 +1,8 @@
+class SignUpPage {
+    get emailField() {return $('#email')}
+    get usernameField() {return $('#login')}
+    get passwordField() {return $('#password')}
+
+}
+
+export default new SignUpPage()
