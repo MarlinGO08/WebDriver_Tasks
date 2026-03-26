98eff88ef5d34353c1b2362a52a5481df0bd8ebf
Olexander <shingm8991677@gmail.com>
2026-03-26 16:22:54 +0200

    Added script for test case GT-002

--- HomeTask_3.js
+++ HomeTask_3.js
@@ -23,4 +23,27 @@ describe('HomeTask_3', () => {
         await button.click()
         await browser.pause(5000)
     })
+
+    it.only('GT-002', async ()=> {
+        await browser.setWindowSize(1440, 1000);
+        await browser.url(BASE_URL)
+        await browser.pause(2000)
+
+        const testData = 'valid@gmail.com'
+        const inputWindow = $('#hero_user_email')
+
+        //Filling input field with our test data
+        await inputWindow.click()
+        await inputWindow.setValue(testData)
+        await browser.pause(3000)
+
+        //Clicking the [Sign up for Github] button 
+        const button = await $('.Primer_Brand__Button-module__Button___lDruK.Primer_Brand__Button-module__Button--primary___xIC7G.Primer_Brand__Button-module__Button--size-medium___EyCyw.CtaForm-primaryAction.CtaFormControl-button.js-hero-action')
+        console.log('The [Sign up for Github] button is clickable: ', await button.isClickable())
+        await button.click()
+        await browser.pause(5000)
+
+        //Verifying we are on the Sign Up page
+        expect(await browser.url('https://github.com/signup'))
+    })
 })
